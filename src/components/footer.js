bf0e5f87e5229f18adcc9436161431c4bd3b9308
Jeff Liknes <jeff.liknes@gmail.com>
2019-10-18 10:32:21 -0600

    added footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,50 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import "../components/styles/footerStyle.scss";
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+// import styled from "styled-components";
+// import "../components/styles/footerStyle.scss";
+// import { Img } from "gatsby-image";
 
-const SpectrumLogoFooter = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-const MailandNumber = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-const FullspectrumWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-class footer extends Component {
-  render() {
-    return (
-      <div>
-        <SpectrumLogoFooter>
-        <div>
-          <a href="mailto:jeff.liknes@gmail.com?subject=DesignbyJeff Project">
-            jeff.liknes@gmail.com
-          </a>
-
-          <div class="DesignbyJeffLogo" />
-        </div>
-          <FullspectrumWrapper>
-            <MailandNumber>
-             
-
-              <a href="mailto:fullspectrumnelson@gmail.com?subject=Full Spectrum Electric Project">
-                fullspectrumnelson@gmail.com
-              </a>
-            </MailandNumber>
-
-            <div class="SpecLogo" />
-          </FullspectrumWrapper>
-        </SpectrumLogoFooter>
-      </div>
-    );
+const Footer = () => {
+  const data = StaticQuery(graphql`
+  query Images {
+    image: file(relativePath: {eq: "unnamed.jpg"}) {
+      id
+      childImageSharp {
+        fluid {
+          src
+          srcSet
+        }
+      }
+    }
   }
-}
+  `)
+  console.log(data);
+   return (
+     <div>
 
-export default footer;
+     </div>
+   )
+  
+   };
+export default Footer;
+
+// <div>
+//   <SpectrumLogoFooter>
+//     <div>
+//       <a href="mailto:jeff.liknes@gmail.com?subject=DesignbyJeff Project">
+//         jeff.liknes@gmail.com
+//       </a>
+
+//       <div class="DesignbyJeffLogo" />
+//     </div>
+//     <FullspectrumWrapper>
+//       <MailandNumber>
+//         <a href="mailto:fullspectrumnelson@gmail.com?subject=Full Spectrum Electric Project">
+//           fullspectrumnelson@gmail.com
+//         </a>
+//       </MailandNumber>
+
+//       <div class="SpecLogo" />
+//     </FullspectrumWrapper>
+//   </SpectrumLogoFooter>
+// </div>;
