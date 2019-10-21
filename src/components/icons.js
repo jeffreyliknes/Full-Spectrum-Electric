5663812fd3becc2b31adbb07af724998ebc9fe68
Jeff Liknes <jeff.liknes@gmail.com>
2019-10-20 21:27:49 -0600

    added icons to front page

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,49 +1,84 @@
 import React from "react";
 import styled from "styled-components";
-import { Fan } from "styled-icons/fa-solid/Fan";
+import { Link } from "gatsby";
+import { HomeAlt } from "styled-icons/boxicons-regular/HomeAlt";
 import { Lightbulb } from "styled-icons/fa-regular/Lightbulb";
 import { Hammer } from "styled-icons/fa-solid/Hammer";
 import { SolarPanel } from "styled-icons/fa-solid/SolarPanel";
 
+// const IconWrapper = styled.div`
+//   width: 40%;
+//   display: flex;
+//   flex-grow: 1;
+//   justify-content: space-around;
+//   width: 100%;
+// `;
 
-const IconWrapper = styled.div`
-width: 40%;
-display: flex;
-flex-grow: 1;
-justify-content: space-around;
-width: 100%;
-`;
-
-
-const GoldFan = styled(Fan)`
+const GoldHome = styled(HomeAlt)`
   color: darkorange;
   flex-grow: 1;
-  
+  width: 30%;
 `;
 const GoldLightbulb = styled(Lightbulb)`
   color: darkorange;
   flex-grow: 1;
-
+  width: 22%;
 `;
 const GoldHammer = styled(Hammer)`
   color: darkorange;
   flex-grow: 1;
-
+  width: 30%;
 `;
 const GoldSolarPanel = styled(SolarPanel)`
   color: darkorange;
   flex-grow: 1;
+  width: 30%;
+`;
 
+const JustifyCenterGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-items: center;
+  grid-gap: 3rem;
+  text-align: center;
+  a {
+      text-decoration: none;
+      color: 
+  }
 `;
 
 const Icon = () => {
   return (
-    <IconWrapper>
-      <GoldFan />
-      <GoldLightbulb />
-      <GoldHammer />
-      <GoldSolarPanel />
-    </IconWrapper>
+    <div>
+      <h2>Services</h2>
+      <JustifyCenterGrid>
+        <Link to="/services">
+          <GoldHome />
+        </Link>
+        <Link to="/services">
+          <GoldLightbulb />
+        </Link>
+        <Link to="/services">
+          <h3>Residential</h3>
+        </Link>
+        <Link to="/services">
+          <h3>Lighting Design</h3>
+        </Link>
+        <Link to="/services">
+          <GoldHammer />
+        </Link>
+        <Link to="/services">
+          <GoldSolarPanel />
+        </Link>
+        <Link to="/services">
+          <h3>Commercial</h3>
+        </Link>
+        <Link to="/services">
+          <h3>Renewable Energy</h3>
+        </Link>
+      </JustifyCenterGrid>
+    </div>
   );
 };
 
