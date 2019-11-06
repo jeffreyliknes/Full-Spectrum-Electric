7f2b66568fec9474dcde4c8bd28be1dda2ba61fd
Jeff Liknes <jeff.liknes@gmail.com>
2019-11-06 12:11:09 -0700

    google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
+import ReactGA from 'react-ga';
 
 const Home = () => (
   <Layout>
     <SEO />
-    <h1>test</h1>
+    <h1>Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
