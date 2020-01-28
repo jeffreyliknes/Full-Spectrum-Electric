7195bae2aa634328cee8d43ec9f8c3b1a9e0b2db
Jeff Liknes <jeff.liknes@gmail.com>
2020-01-28 20:53:00 +0800

    made all of the img rounded

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ import SEO from "../components/seo";
 const Home = () => (
   <Layout>
     <SEO />
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
   </Layout>
 )
 
